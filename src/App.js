d449ec9fc00977fca0604ebb24a0ad1f16a3c502
Duy Nguyen <nguyenanhduy0400@gmail.com>
2021-07-03 13:05:49 +0700

    [New] TodoList Form using useState()

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.scss";
+import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
 function App() {
@@ -14,11 +15,24 @@ function App() {
     setTodoList(updatedTodoList);
   }
 
+  function handleTodoFormSubmit(formValues) {
+    formValues && alert("Form submitted successfully!");
+    const updatedTodoList = [...todoList];
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValues,
+    };
+    updatedTodoList.push(newTodo);
+    setTodoList(updatedTodoList);
+  }
+
   return (
     <div className="App">
       <h1>React hooks - TodoList</h1>
 
       <TodoList todos={todoList} onTodoClick={handleTodoClick} />
+
+      <TodoForm onSubmit={handleTodoFormSubmit} />
     </div>
   );
 }
