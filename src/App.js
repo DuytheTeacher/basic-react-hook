e3b47041db46e460a0998b6c6c5b8ad091135c03
Duy-Nguyen <nguyenanhduy0400@gmail.com>
2021-07-03 15:14:25 +0700

    [New] Call API with useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.scss";
+import PostList from "./components/PostList";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
@@ -9,6 +10,25 @@ function App() {
     { id: 2, title: "We love Easy Frontend!" },
     { id: 3, title: "They love Easy Frontend!" },
   ]);
+
+  const [postList, setPostList] = useState([]);
+
+  useEffect(() => {
+    async function fetchPostList() {
+      try {
+        const requestUrl =
+          "http://js-post-api.herokuapp.com/api/posts?_limit=10&_page=1";
+        const response = await fetch(requestUrl);
+        const responseJSON = await response.json();
+
+        const { data } = responseJSON;
+        setPostList(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchPostList();
+  }, []);
 
   function handleTodoClick(todo) {
     const updatedTodoList = todoList.filter((item) => item.id !== todo.id);
@@ -30,9 +50,10 @@ function App() {
     <div className="App">
       <h1>React hooks - TodoList</h1>
 
-      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
+      {/* <TodoList todos={todoList} onTodoClick={handleTodoClick} />
 
-      <TodoForm onSubmit={handleTodoFormSubmit} />
+      <TodoForm onSubmit={handleTodoFormSubmit} /> */}
+      <PostList posts={postList} />
     </div>
   );
 }
